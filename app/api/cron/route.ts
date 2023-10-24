58afbbf600b9e8251f1cd869b4f4bb901403922f
rrrx-4 <69399883+rrrx-4@users.noreply.github.com>
2023-10-24 17:08:18 +0530

    fixed err

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -7,7 +7,7 @@ import { generateEmailBody, sendEmail } from "../../../lib/nodemailer";
 import { scrapeAmazonProduct } from "../../../lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "../../../lib/utils";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 5; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
